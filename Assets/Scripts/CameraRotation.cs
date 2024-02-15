92bb25b4495f48f7be60051969562c5696897313
Toxa <antonplay881@mail.ru>
2024-02-15 14:11:29 +1200

    RotationSpeed

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float RotationSpeed;
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime, 0);
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed, 0);
     }
 }
