cf09dde6e394769c5b44b9a1fb59f2a6a95de63b
Toxa <antonplay881@mail.ru>
2024-02-19 04:45:02 +1200

    Finally fixed camera

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -22,7 +22,10 @@ public class CameraRotation : MonoBehaviour
         transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X"), 0);
 
         var newAngleX = CameraAxisTransform.localEulerAngles.x - Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse Y");
-        //newAngleX = Mathf.Clamp(newAngleX, minAngle, maxAngle);
+        if (newAngleX > 180)
+            newAngleX -= 360;
+
+        newAngleX = Mathf.Clamp(newAngleX, minAngle, maxAngle);
 
         CameraAxisTransform.localEulerAngles = new Vector3(newAngleX, 0, 0);
     }
