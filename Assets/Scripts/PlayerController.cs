5f8dcad94bb4054d2fbcf664213294897849e72f
Toxa <antonplay881@mail.ru>
2024-02-22 08:01:55 +1200

    WASD Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     public float gravity = 9.8f;
     public float jumpForce;
+    public float speed;
 
+    private Vector3 _moveVector;
     private float _fallVelocity = 0;
 
     private CharacterController _characterController;
@@ -18,6 +20,28 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+
+        //Movement
+        _moveVector = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+        }
+
+        //Jump
         if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
         {
             _fallVelocity = -jumpForce;
@@ -26,6 +50,11 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+
+
+        _characterController.Move(_moveVector * speed * Time.fixedDeltaTime);
+
+        //Fall and Jump
         _fallVelocity += gravity * Time.fixedDeltaTime;
         _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
     }
