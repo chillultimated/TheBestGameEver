2a654714ab75e2303761c0f7ed444feef44d512b
Toxa <antonplay881@mail.ru>
2024-03-24 14:06:07 +1200

    Update EnemyAI.cs

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -2,24 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
 public class EnemyAI : MonoBehaviour
 {
     public List<Transform> patrolPoints;
 
     private NavMeshAgent _navMeshAgent;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
+    {
+        InitComponentLinks();
+        PickNewPatrolPoint();
+    }
+    private void InitComponentLinks()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
-
-        _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-
+        PatrolUpdate();
+    }  
+    private void PatrolUpdate()
+    {
+        if (_navMeshAgent.remainingDistance == 0)
+        {
+            PickNewPatrolPoint();
+        }
+    }
+    private void PickNewPatrolPoint()
+    {
+      _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
     }
 }
